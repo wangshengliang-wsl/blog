82f0426ab109b3b720a8807227e8664507965fe3
wangshengliang-wsl <1710642275@qq.com>
2025-11-23 21:46:28 +0800

    feat: change font

--- ec.config.mjs
+++ ec.config.mjs
@@ -19,11 +19,11 @@ export default defineEcConfig({
 
   /* Styles */
   styleOverrides: {
-    uiFontFamily: "'DM Mono', 'Input Mono', 'Fira Code', 'monospace'",
+    uiFontFamily: "'Input Mono', 'Fira Code', 'monospace'",
     uiFontSize: '1em',
     codeBackground: (context) =>
       context.theme.name === 'vitesse-dark' ? '#0e0e0e' : '#fafafa',
-    codeFontFamily: "'DM Mono', 'Input Mono', 'Fira Code', 'monospace'",
+    codeFontFamily: "'Input Mono', 'Fira Code', 'monospace'",
     codeFontSize: '14.72px',
     codeLineHeight: '1.4',
     codePaddingBlock: '0.8571429em',
