e1dea216a2f144bf0d275dd6d0b6bb26549b673f
wangshengliang-wsl <1710642275@qq.com>
2025-11-29 12:36:17 +0800

    fix: update NODE_OPTIONS in build script to use a safer default memory limit of 4096MB if not set

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-export NODE_OPTIONS='--max-old-space-size=12288'
+# Use environment variable if set, otherwise use a reasonable default
+# Vercel has ~4GB memory, so 4096MB is safer
+export NODE_OPTIONS="${NODE_OPTIONS:---max-old-space-size=4096}"
 exec astro build "$@"
